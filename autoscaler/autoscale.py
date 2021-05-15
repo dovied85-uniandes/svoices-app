07588cf0566ad4b7c7e8326940ad111291a48d10
dovied85-uniandes <78886805+dovied85-uniandes@users.noreply.github.com>
2021-05-14 19:10:51 -0500

    Update autoscale.py

--- autoscaler/autoscale.py
+++ autoscaler/autoscale.py
@@ -49,7 +49,7 @@ def get_queue_size():
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', seconds=os.environ["AUTOSCALING_INTERVAL"])
 def job():
     queue_size = get_queue_size()
 
