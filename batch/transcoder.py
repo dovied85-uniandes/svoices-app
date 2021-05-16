01a2ff247b7bffbdac90e57f30a732ee924e9006
dovied85-uniandes <78886805+dovied85-uniandes@users.noreply.github.com>
2021-05-16 15:17:31 -0500

    Update transcoder.py

--- batch/transcoder.py
+++ batch/transcoder.py
@@ -1,7 +1,8 @@
+#from dotenv import load_dotenv
+#load_dotenv()   # in local: .env need to load before any of our modules
+
 from celery import Celery
 from celery.utils.log import get_task_logger
-
-from dotenv import load_dotenv
 
 from batch.emails import get_email_client
 
@@ -9,18 +10,12 @@ from batch.utils import get_from_env
 
 import boto3
 import json
+import math
 import os
 import subprocess
+import time
 
-root_dir = get_from_env(envvar="AUDIO_FOLDER", default="var/svoice/media/audios")
-
-sqs_queue_region = os.environ["AWS_SQS_REGION"]
-sqs_queue_access_key_id = os.environ["AWS_SQS_ACCESS_KEY_ID"]
-sqs_queue_access_key_secret = os.environ["AWS_SQS_ACCESS_KEY_SECRET"]
-sqs_queue_name = os.environ["AWS_SQS_QUEUE_NAME"]
-
-queue = boto3.resource('sqs', region_name=sqs_queue_region, aws_access_key_id=sqs_queue_access_key_id, aws_secret_access_key=sqs_queue_access_key_secret)\
-    .get_queue_by_name(QueueName=sqs_queue_name)
+audios_dir = get_from_env(envvar="AUDIO_FOLDER", default="var/svoice/media/audios")
 
 s3_bucket_region = os.environ["AWS_S3_REGION"]
 s3_access_key_id = os.environ["AWS_S3_ACCESS_KEY_ID"]
@@ -39,106 +34,118 @@ table = boto3.resource('dynamodb', region_name=dynamodb_table_region,
     aws_access_key_id=dynamodb_access_key_id, aws_secret_access_key=dynamodb_access_key_secret).Table(dynamodb_table_name)
 
 def create_batch():
-    global BATCH_SIZE
-    BATCH_SIZE = get_from_env(envvar="BATCH_SIZE", default=30)
     batch_name = get_from_env(envvar="BATCH_NAME", default="transcoder")
-    broker_url = get_from_env(envvar="TASK_BROKER", desc="No se pudo configurar el broker de mensajería")
-    return Celery(batch_name , broker = broker_url)
+    sqs_queue_region = get_from_env(envvar="AWS_SQS_REGION", desc="Debe especificar la cola de trabajo")
+    sqs_polling_interval = int(get_from_env(envvar="AWS_SQS_POLLING_INTERVAL", default="60"))
+    sqs_long_polling = int(get_from_env(envvar="AWS_SQS_LONG_POLLING", default="20"))
+    sqs_visibility_timeout = int(get_from_env(envvar="AWS_SQS_VISIBILITY", default="60"))
+    sqs_queue_url = get_from_env(envvar="AWS_SQS_QUEUE_URL", desc="Debe especificar la url de la cola de trabajo")
+    sqs_access_key_id = get_from_env(envvar="AWS_SQS_ACCESS_KEY_ID", desc="Debe especificar el id de la llave para conectarse a la cola de trabajo")
+    sqs_access_key_secret = get_from_env(envvar="AWS_SQS_ACCESS_KEY_SECRET", desc="Debe especificar el secreto de la llave para conectarse a la cola de trabajo")
+    broker_url = f"sqs://{sqs_access_key_id}:{sqs_access_key_secret}@"
 
-def query_voices_to_process():
-    logger.info(f"Fetching the next batch of {BATCH_SIZE} audios to process")
-
-    messages = queue.receive_messages(MessageAttributeNames=['All'], MaxNumberOfMessages=int(BATCH_SIZE))
-    logger.info(f"Found {len(messages)} to convert")
-    return messages
+    app = Celery(batch_name, broker = broker_url)
+    app.conf.update(
+        broker_transport_options = {
+            "region": sqs_queue_region,
+            "polling_interval": sqs_polling_interval,
+            "wait_time_seconds": sqs_long_polling,
+            "visibility_timeout": sqs_visibility_timeout,
+            "predefined_queues": {
+                "celery": {
+                    "url": sqs_queue_url,
+                    "access_key_id": sqs_access_key_id,
+                    "secret_access_key": sqs_access_key_secret
+                }
+            }
+        }
+    )
+    return app
 
 def transcode_voice(voice, media_dir):
-    voice = json.loads(voice.body)
-    rel_source_path = voice['rel_source_path']
-    rel_target_path = voice['rel_target_path']
+    # Desgargar el archivo de s3
+    logger.debug("Descargando archivo de audio")
+    rel_source_path = voice["rel_source_path"]
     abs_source_path = os.path.join(media_dir, rel_source_path)
-    abs_target_path = os.path.join(media_dir, rel_target_path)
-
-    logger.info(f"Convirtiendo archivo '{abs_source_path}' a: {abs_target_path}")
-
-    # convert:
-    error_message = f"No se pudo convertir el archivo: {abs_source_path}"
-
     s3resource.meta.client.download_file(s3_bucket_name, abs_source_path, 'audio_original')
-
+    
+    # Convertir el archivo descargado:
+    error_message = f"No se pudo convertir el archivo: {abs_source_path}"
     try:
-        result = subprocess.run(["ffmpeg", "-y", "-i", 'audio_original', "-f", "mp3", 'audio_convertido'])
+        rel_target_path = voice["rel_target_path"]
+        abs_target_path = os.path.join(media_dir, rel_target_path)
+        logger.info(f"Convirtiendo archivo '{abs_source_path}' a: {abs_target_path}")
+        result = subprocess.run(["/usr/bin/ffmpeg", "-y", "-i", 'audio_original', "-f", "mp3", 'audio_convertido'])
         if result.returncode == 0:
-            s3resource.meta.client.upload_file('audio_convertido', s3_bucket_name, abs_target_path)
+            s3resource.meta.client.upload_file("audio_convertido", s3_bucket_name, abs_target_path)
             return voice
     except Exception as e:
         error_message = f"{error_message}: {str(e)}"
 
-    logger.error(f"No se pudo convertir el archivo: {abs_source_path}")
+    logger.error(error_message)
     return None
-
-def transcode_voices(voices_to_process):
-    voices_processed = map(lambda voice: transcode_voice(voice, root_dir), voices_to_process)
-    voices_to_update = filter(lambda voice: voice is not None, voices_processed)
-    return list(voices_to_update)
     
-def mark_converted(converted_voices):
-    num_converted = len(converted_voices)
-    if num_converted == 0:
-        logger.info("Ninguna voz fue convertida en esta iteración.")
-        return 0
+def mark_converted(converted_voice):
+    if converted_voice is None:
+        return False
 
-    logger.info(f"{num_converted} voces fueron convertidas. Actualizando su estado en la base de datos.")
-    # update status
+    logger.info("Marcando como 'convertida' la voz en la base de datos.")
     with table.batch_writer() as batch:
-        for converted_voice in converted_voices:
-            table.update_item(Key={'pk': converted_voice['pk'], 'sk': converted_voice['sk']}, AttributeUpdates={'status': {'Value': 'Convertida', 'Action': 'PUT'}})
+        voice_key = {"pk": converted_voice["pk"], "sk": converted_voice["sk"]}
+        status_attr = {"status": {"Value": "Convertida", "Action": "PUT"}}
+        table.update_item(Key=voice_key, AttributeUpdates=status_attr)
+    return True
 
-    return num_converted
-
-def notify_authors(converted_voices):
-    logger.info(f"Notifying voice authors that their voices were converted.")
+def notify_author(voice):
+    logger.info("Enviando correo de notificación de voz convertida.")
     smtp_client = get_email_client(connect=True, logger=logger)
     
-    for voice in converted_voices:
-        author_email = voice['author_email']
-        author_first_name = voice['author_first_name']
-        author_last_name = voice['author_last_name']
-        voice_file_name = voice['rel_source_path'].rsplit("/", 1)[1]
-        msg_text = f"Concursante {author_first_name} {author_last_name}:\n Queremos avisarle que su audio {voice_file_name} ya ha sido convertido."
-        try:
-            smtp_client.send_email(author_email, "Audio convertido", msg_text)
-        except Exception as e:
-            logger.error(f"Unable to send notification to '{author_email}': {e}")
+    author_email = voice['author_email']
+    author_first_name = voice['author_first_name']
+    author_last_name = voice['author_last_name']
+    voice_file_name = voice['rel_source_path'].rsplit("/", 1)[1]
+    msg_text = f"Concursante {author_first_name} {author_last_name}:\n Queremos avisarle que su audio {voice_file_name} ya ha sido convertido."
+    try:
+        smtp_client.send_email(author_email, "Audio convertido", msg_text)
+    except Exception as e:
+        logger.error(f"No se pudo enviar correo de notificación a '{author_email}': {e}")
 
     smtp_client.disconnect()
 
-load_dotenv()
 app = create_batch()
 logger = get_task_logger(__name__)
 
-task_period = float(get_from_env(envvar="TASK_PERIOD", default=60))
-app.conf.beat_schedule = {
-    "Convert-audio-files": {
-        "task": "batch.transcoder.convert_audios",
-        "schedule": task_period
-    }
-}
+def handle_error(error, start_time):
+    """
+    This method favors consistency over throughput and will wailt until current_time - start_time > visibility
+    to give a chance to other workers to pickup the failed voice.
+    """
+    current_time = time.time()
+    elapsed_time = math.floor(current_time - start_time)
+    visibility_timeout = int(get_from_env(envvar="AWS_SQS_VISIBILITY", default="60"))
+    diff_time = visibility_timeout - elapsed_time
+    if diff_time > 0:
+        logger.warn(f"Sleeping for {diff_time} seconds to avoid celery acknowledging this failed task")
+        time.sleep(diff_time)
 
-@app.task
-def convert_audios():
+@app.task()
+def convert_audio(voice):
+    """
+    This task may complete successfully even if no convertion can be performed, so check the
+    logs if you see tasks completing successfully but no voice files being converted.
+    @param voice a dictionary containing the properties of the voice to be configured.
+    @return "DONE with SUCCESS" when the task completes without errors and "DONE with
+    ERRORS" otherwise.
+    """
+    start_time = time.time()
     try:
-        voices_to_process = query_voices_to_process()
-        transcoded_voices = transcode_voices(voices_to_process)
-        voices_updated = mark_converted(transcoded_voices)
+        converted_voice = transcode_voice(voice, audios_dir)
+        voice_updated = mark_converted(converted_voice)
 
-        if voices_updated > 0:
-            if os.environ["NOTIFY_AUTHORS"] == "TRUE":
-                notify_authors(transcoded_voices)
-            entries = [{'Id': str(ind), 'ReceiptHandle': msg.receipt_handle} for ind, msg in enumerate(voices_to_process) if json.loads(msg.body)['id'] in [v['id'] for v in transcoded_voices]]
-            queue.delete_messages(Entries=entries)
-
+        if voice_updated and get_from_env(envvar="NOTIFY_AUTHORS", default="FALSE") == "TRUE":
+            notify_author(converted_voice)
         return "DONE with SUCCESS"
     except Exception as e:
-        logger.error(f"Ocurrió un error durante la ejecución de la tarea: {str(e)}")
+        logger.error(f"Error inesperado durante la ejecución de la tarea: {str(e)}")
+        handle_error(e, start_time)
         return "DONE with ERRORS"
